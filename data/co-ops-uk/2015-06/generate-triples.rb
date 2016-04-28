2ac3b9e9c4e5ae26ea08a01ba9d14c25d35e7c88
Matt Wallis <matt.wallis@tickpea.com>
2016-04-28 14:39:38 +0100

    Can now generate collection.rdf, listing all of the separate SSE initiatives.

--- data/co-ops-uk/2015-06/generate-triples.rb
+++ data/co-ops-uk/2015-06/generate-triples.rb
@@ -4,7 +4,51 @@ require 'rdf/vocab'
 
 $input_csv, $output_dir, $uri_base  = $ARGV
 
+def save_rdf(opts)
+  filename = "#{opts[:dir] || $output_dir}#{opts[:basename]}.rdf"
+  RDF::RDFXML::Writer.open(filename, prefixes: opts[:prefixes]) {|writer| writer << opts[:graph] }
+end
+def save_ttl(opts)
+  filename = "#{opts[:dir] || $output_dir}#{opts[:basename]}.ttl"
+  RDF::Turtle::Writer.open(filename, prefixes: opts[:prefixes]) {|writer| writer << opts[:graph] }
+end
+
+class Collection < Array	# of Initiatives
+  def make_graph
+    graph = RDF::Graph.new
+    # TODO: should we be using parseType="Collection"?
+    # https://www.w3.org/TR/rdf-syntax-grammar/#section-Syntax-parsetype-Collection
+    # Not sure how to do that with RDF.rb.
+    graph.insert([uri, RDF.type, collection_class])
+    each {|i|	# each initiative in the collection
+      graph.insert([uri, contains, i.uri])
+    }
+    return graph
+  end
+  def graph
+    make_graph
+  end
+  def prefixes
+    {}
+  end
+  def basename
+    "collection"
+  end
+  private
+  def collection_class
+    RDF::URI("#{$uri_base}SSECollection")
+  end
+  def contains
+    RDF::URI("#{$uri_base}contains")
+  end
+  def uri
+    RDF::URI("#{$uri_base}#{basename}")
+  end
+
+end
+
 class Initiative
+  attr_reader :id
   @@last_id = 0
   @@essglobal = RDF::Vocabulary.new("http://purl.org/essglobal/vocab/")
   def initialize(opts)
@@ -19,30 +63,29 @@ class Initiative
     @@last_id += 1
     @id = @@last_id
   end
-  def save_rdf(output_dir)
-    filename = "#{output_dir}#{@id}.rdf"
-    prefixes = {vcard: RDF::Vocab::VCARD.to_uri.to_s, essglobal: essglobal.to_uri.to_s, gr: RDF::Vocab::GR.to_uri.to_s}
-    RDF::RDFXML::Writer.open(filename, prefixes: prefixes) {|writer| writer << make_graph }
+  def graph
+    make_graph
   end
-  def save_ttl(output_dir)
-    filename = "#{output_dir}#{@id}.ttl"
-    prefixes = {vcard: RDF::Vocab::VCARD.to_uri.to_s, essglobal: essglobal.to_uri.to_s, gr: RDF::Vocab::GR.to_uri.to_s}
-    RDF::Turtle::Writer.open(filename, prefixes: prefixes) {|writer| writer << make_graph }
+  def prefixes
+    {vcard: RDF::Vocab::VCARD.to_uri.to_s, essglobal: essglobal.to_uri.to_s, gr: RDF::Vocab::GR.to_uri.to_s}
+  end
+  def basename	# for output files
+    id
+  end
+  def uri
+    RDF::URI("#{$uri_base}#{@id}")
   end
   private
   def essglobal
     @@essglobal
   end
-  def uri
-    "#{$uri_base}#{@id}"
-  end
   def make_graph
-    initiative = RDF::URI(uri)
     graph = RDF::Graph.new
-    graph.insert([initiative , RDF.type, essglobal["SSEInitiative"]])
-    graph.insert([initiative, RDF::Vocab::GR.name, @defn["name"]])
-    graph.insert([initiative, essglobal.hasAddress, make_address(graph)])
-    graph.insert([initiative, essglobal.legalForm, RDF::URI("http://www.purl.org/essglobal/standard/legal-form/L2")])
+    graph.insert([uri, RDF.type, essglobal["SSEInitiative"]])
+    graph.insert([uri, RDF::Vocab::GR.name, @defn["name"]])
+    graph.insert([uri, essglobal.hasAddress, make_address(graph)])
+    graph.insert([uri, essglobal.legalForm, RDF::URI("http://www.purl.org/essglobal/standard/legal-form/L2")])
+    return graph
   end
   def make_address(graph)
     addr = RDF::Node.new
@@ -53,6 +96,7 @@ class Initiative
   end
 end
 
+collection = Collection.new
 CSV.foreach($input_csv, :encoding => "ISO-8859-1", :headers => true) do |row|
   # There may be rows for previous years' accounts - we ignore these:
   if row["Is Most Recent"] == "1"
@@ -61,7 +105,15 @@ CSV.foreach($input_csv, :encoding => "ISO-8859-1", :headers => true) do |row|
       "postal-code" => row["Registered Postcode"],
       "country-name" => row["UK Nation"]
     )
-    initiative.save_rdf($output_dir)
-    initiative.save_ttl($output_dir)
+    graph = initiative.graph
+    save_rdf(:basename => initiative.basename, :prefixes => initiative.prefixes, :graph => graph);
+    save_ttl(:basename => initiative.basename, :prefixes => initiative.prefixes, :graph => graph);
+    #initiative.save_rdf($output_dir)
+    #initiative.save_ttl($output_dir)
+    collection << initiative
   end
 end
+graph = collection.graph
+save_rdf(:basename => collection.basename, :prefixes => collection.prefixes, :graph => graph);
+save_ttl(:basename => collection.basename, :prefixes => collection.prefixes, :graph => graph);
+#collection.save_rdf($output_dir)
