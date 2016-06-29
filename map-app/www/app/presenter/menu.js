4241da952c2af69d54f7c558115fda8c25520e7f
Matt Wallis <matt.wallis@tickpea.com>
2016-06-29 09:27:41 +0100

    Experimenting with different hard-coded RDF URIs.

--- map-app/www/app/presenter/menu.js
+++ map-app/www/app/presenter/menu.js
@@ -4,8 +4,11 @@ define( ["model/rdf"], function(rdfModel) {
 	var view;
 
 	function loadRdfFromUri() {
-		var uri = "http://data.solidarityeconomics.org/doc/experimental/co-ops-uk.rdf";
+		//var uri = "http://data.solidarityeconomics.org/doc/experimental/co-ops-uk.rdf";
 		//var uri = "http://data.solidarityeconomics.org/id/experimental/co-ops-uk";
+		var uri = "http://data.solidarityeconomics.org/id/experimental/co-ops-uk/R008807";
+		//var uri = "http://data.ordnancesurvey.co.uk/id/postcodeunit/OX11BP";
+		//var uri = "http://data.ordnancesurvey.co.uk/doc/postcodeunit/OX11BP.rdf";
 		console.log("Load data from URI - hard-coded URI:");
 		console.log(uri);
 		rdfModel.loadUri(uri);
