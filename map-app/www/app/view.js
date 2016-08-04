d48cf841f77608a4af58fbf5c7dea68277290b38
Matt Wallis <matt.wallis@tickpea.com>
2016-08-04 15:42:51 +0100

    Introduce About button for viewing docs on github.

--- map-app/www/app/view.js
+++ map-app/www/app/view.js
@@ -1,8 +1,12 @@
 // This is the place where the various views are pulled into the application.
-define(["view/map", "view/menu", "view/tabs"], function(map, menu, tabs) {
+define(["d3", "view/map", "view/menu", "view/tabs"], function(d3, map, menu, tabs) {
 	"use strict";
 
 	function init() {
+		d3.select("#about-btn")
+		.attr("title", "See information about this app in new tab")
+		.on("click", function() { window.open("https://github.com/p6data-coop/ise-linked-open-data/blob/master/map-app/README.md", "_blank");});
+
 		map.init();
 		menu.init();
 
