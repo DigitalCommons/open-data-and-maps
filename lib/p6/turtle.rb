63db732bae982e38129693313d6c2734c28b95d6
Matt Wallis <matt.wallis@tickpea.com>
2016-10-19 14:35:33 +0100

    Automatically include standard prefixes when generating RDF/XML and Turtle

--- lib/p6/turtle.rb
+++ lib/p6/turtle.rb
@@ -7,7 +7,7 @@ module P6
     def Turtle.save_file(opts)	# named params :graph, :prefixes, :dir, :basename, :filename
       # Only use :dir and :basename if :filename is missing:
       filename = opts[:filename] || P6::File.name(opts[:dir], opts[:basename], "ttl")
-      RDF::Turtle::Writer.open(filename, prefixes: opts[:prefixes]) {|writer| writer << opts[:graph] }
+      RDF::Turtle::Writer.open(filename, standard_prefixes: true, prefixes: opts[:prefixes]) {|writer| writer << opts[:graph] }
       filename
     end
   end
