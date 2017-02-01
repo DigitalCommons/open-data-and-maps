9a12fe5e68d74f32e09e5cc1e4cdec9a32babac1
Matt Wallis <matt.wallis@tickpea.com>
2017-02-01 13:00:00 +0000

    Avoid use of global vars across files
    Issue #22

--- map-app/www/services/display_similar_companies/main.php
+++ map-app/www/services/display_similar_companies/main.php
@@ -5,7 +5,57 @@
 <body>
 
 <?php
-if($_GET){$input = htmlspecialchars($_GET["company"]);
+function report_error_and_die($msg)
+{
+	$result = array();
+	// API response uses JSend: https://labs.omniti.com/labs/jsend
+	$result["status"] = "error";
+	$result["message"] = $msg;
+	if (function_exists('http_response_code')) {
+		http_response_code(500);
+	}
+	else {
+		// TODO - use header() function
+	}
+	echo json_encode($result);
+	exit(1);
+}
+
+function request($url){
+
+	// is curl installed?
+	if (!function_exists('curl_init')){ 
+		report_error_and_die("PHP can't find function curl_init. Is CURL installed?");
+	}
+	$ch= curl_init();
+
+	$headers = array(
+		"Accept: application/sparql-results+json"
+	);
+
+	// set request url
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+	// return response, don't print/echo
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+	//curl_setopt($ch, CURLOPT_TIMEOUT, 300);
+	//curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
+
+	// See curl docs: http://www.php.net/curl_setopt
+
+	$response = curl_exec($ch);
+	if (curl_errno($ch)) { 
+		report_error_and_die("PHP curl_exec produces error: " . curl_error($ch)); 
+	}
+
+	curl_close($ch);
+
+	return $response;
+}
+if ($_GET) {
+	$input = htmlspecialchars($_GET["company"]);
 
 	#Generate $query with $input as argument
 	include 'competitors-query.php';
@@ -14,7 +64,9 @@ if($_GET){$input = htmlspecialchars($_GET["company"]);
 	$endpoint = trim(file_get_contents('endpoint.txt'));
 
 	#Execute query with arguments, $query and $endpoint, returning json object, $response
-	include 'request-results.php';
+	$requestURL = $endpoint.'?' .'query='.urlencode($query);
+	$response = request($requestURL);
+	//include 'request-results.php';
 
 	echo '<p>'.$input.'</p>';
 	#Quick Description of SIC Label 
@@ -27,8 +79,6 @@ if($_GET){$input = htmlspecialchars($_GET["company"]);
 	$parameters	= array('name','postcode');
 	$json = $response;
 	include 'create-table.php';
-	
-
 }
 ?>
 
