25bc5b23996e920a2279cf2ec6b461cecaca1583
U-Lenovo-PC\Dan <dan@parecco.co.uk>
2017-06-26 16:49:01 +0100

    bug 310 profile.php now has fixed table layout

--- local-oxford/auth/profile.php
+++ local-oxford/auth/profile.php
@@ -67,6 +67,12 @@
     <p>View and change your information below</p>
     <form action="change-data.php" method="POST" id="form"></form>
     <table>
+        <colgroup>
+    <col style="width:20%;">
+    <col style="width:30%;">
+    <col style="width:40%;">
+    <col style="width:10%;">
+        </colgroup>
         <tr>
             <th>Field</th>
             <th>Current Info</th>
