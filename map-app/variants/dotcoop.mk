bbe9051a8c8dfebc90eb439d280e299de6ba31b5
Matt Wallis <matt.wallis@tickpea.com>
2018-05-24 12:48:25 +0100

    dotcoop map now configured #67

--- map-app/variants/dotcoop.mk
+++ map-app/variants/dotcoop.mk
@@ -5,7 +5,7 @@ SERVER := ise-0-admin
 # Base deployment directory on server (must pre-exist):
 SERVER_BASE_DIR := /var/www/html/
 
-# Directory under SERVER_DIR to deploy to (will be created during deployment):
+# Directory under SERVER_BASE_DIR to deploy to (will be created during deployment):
 SERVER_APP_SUBDIR := maps/$(variant)/
 
 # JSON file for this configuration, to be loaded into the running map-app:
