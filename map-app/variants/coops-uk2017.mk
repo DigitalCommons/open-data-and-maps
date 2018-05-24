b0af6cb7689a7a8509c41cf829e275282b297bfc
Matt Wallis <matt.wallis@tickpea.com>
2018-05-24 12:57:25 +0100

    Configuration for coops-uk 2016/7 #67

---
+++ map-app/variants/coops-uk2017.mk
@@ -0,0 +1,12 @@
+
+# Server to deploy to:
+SERVER := ise-0-admin
+
+# Base deployment directory on server (must pre-exist):
+SERVER_BASE_DIR := /var/www/html/
+
+# Directory under SERVER_BASE_DIR to deploy to (will be created during deployment):
+SERVER_APP_SUBDIR := maps/$(variant)/
+
+# JSON file for this configuration, to be loaded into the running map-app:
+SRC_CONFIG_JSON := variants/$(variant)/config.json
