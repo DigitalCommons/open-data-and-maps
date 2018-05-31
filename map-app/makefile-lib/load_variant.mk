a1d2f1b8356cde046c195fe5cad6446d95d62d47
Matt Wallis <matt.wallis@tickpea.com>
2018-05-31 12:12:59 +0100

    dotcoop-coopsukk variant now available #67

--- map-app/makefile-lib/load_variant.mk
+++ map-app/makefile-lib/load_variant.mk
@@ -11,7 +11,9 @@ $(info ERROR: The make command must set the variant using:)
 $(info $(space)    make variant=<name>)
 $(info This will cause the file <name>.mk to be included from the directory '$(VARIANTS_DIR)'.)
 $(info Looking at .mk files in '$(VARIANTS_DIR)', possible values for <name> are:)
+$(info )
 $(info $(space)    $(possible_variants))
+$(info )
 $(error Stopping due to error.)
 endif
 
