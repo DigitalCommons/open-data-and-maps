b82545d10824ab434cfb3c4c9f8042d8353765e8
Matt Wallis <matt.wallis@tickpea.com>
2018-07-19 15:47:19 +0100

    About button now works on Sidebar, #73
    Search box now has event handler, #72

--- map-app/www/app/view.js
+++ map-app/www/app/view.js
@@ -7,6 +7,18 @@ define(["d3", "view/map"], function(d3, map) {
 		.attr("title", "See information about this app in new tab")
 		.on("click", function() { window.open("https://github.com/p6data-coop/ise-linked-open-data/blob/master/map-app/README.md", "_blank");});
 
+		d3.select("#search-form")
+		.on("submit", function() {
+			// By default, submitting the form will cause a page reload!
+			d3.event.preventDefault();
+
+			//d3.event.stopPropagation();
+			var searchText = d3.select("#search-box").property("value");
+			console.log("Search submitted: [" + searchText + "]");
+			//console.log(d3.event);
+			//console.log(d3.select("#search-box").property("value"));
+		});
+
 		map.init();
 	}
 	var pub = {
