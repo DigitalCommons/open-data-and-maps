c71a2b3bf4d8edcde5c6bbb295c0fdb5533d68e7
Matt Wallis <matt.wallis@tickpea.com>
2018-09-11 18:10:42 +0100

    Configure a map for the newcastle pilot
    SolidarityEconomyAssociation/open-data-and-maps-outreach#90

---
+++ map-app/variants/internal-newcastle-pilot.mk
@@ -0,0 +1,19 @@
+# This variant is for testing.
+# It's a variant of the newcastle-pilot variant
+PARENT_VARIANT := newcastle-pilot
+
+# Server to deploy to:
+SERVER := ise-0-matt
+
+# Base deployment directory on server (must pre-exist):
+SERVER_BASE_DIR := /var/www/html/internal/maps/
+
+# Directory under SERVER_BASE_DIR to deploy to (will be created during deployment):
+SERVER_APP_SUBDIR := $(PARENT_VARIANT)/
+
+# URL where map will be found after deployment:
+DEPLOYED_MAP_URL := https://internal.solidarityeconomy.coop/maps/$(SERVER_APP_SUBDIR)
+
+# JSON file for this configuration, to be loaded into the running map-app:
+SRC_CONFIG_JSON := variants/$(PARENT_VARIANT)/config.json
+
