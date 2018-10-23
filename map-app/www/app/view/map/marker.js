bb7d7a49ed73d7543ae56115c158832e9eefc595
Matt Wallis <matt.wallis@tickpea.com>
2018-10-23 12:40:14 +0100

    Remove commented-out old code

--- map-app/www/app/view/map/marker.js
+++ map-app/www/app/view/map/marker.js
@@ -15,22 +15,15 @@ define(["leaflet", "leafletMarkerCluster", "leafletAwesomeMarkers", "view/base",
 	// http://fortawesome.github.io/Font-Awesome/icons/
 	const dfltOptions = {prefix: "fa"};	// "fa" selects the font-awesome icon set (we have no other)
 
-	//proto.create = function(map, cluster, latlng, options, eventHandlers) {
 	proto.create = function(map, initiative) {
 
-		//var popuptext = options.popuptext || hovertext || "Sorry. Popup text missing!";
-		//var hovertext = options.hovertext || "Hover text goes here.";
 		const hovertext = this.presenter.getHoverText(initiative);
-		//const popuptext = hovertext || "Sorry. Popup text missing!";
 
 		// options argument overrides our default options:
-		//const opts = Object.assign(dfltOptions, options);
-		//const opts = Object.assign(dfltOptions, this.presenter.getIconOptions(initiative));
 		const opts = Object.assign(dfltOptions, {
 			icon: this.presenter.getIcon(initiative),
 			popuptext: this.presenter.getPopupText(initiative),
 			hovertext: this.presenter.getHoverText(initiative),
-			//cluster: this.presenter.getTrue(initiative),
 			cluster: true,
 			markerColor: this.presenter.getMarkerColor(initiative)
 		});
@@ -38,23 +31,17 @@ define(["leaflet", "leafletMarkerCluster", "leafletAwesomeMarkers", "view/base",
 		// Note that the dependency between AwesomeMarkers and leaflet is expressed as a 
 		// requireJS shim config in our main requireJS configuration.
 		const icon = leaflet.AwesomeMarkers.icon(opts);
-		//this.marker = leaflet.marker(latlng, {icon: icon, title: hovertext});
 		this.marker = leaflet.marker(this.presenter.getLatLng(initiative), {icon: icon, title: hovertext});
 		// maxWidth helps to accomodate big font, for presentation purpose, set up in CSS
 		// maxWidth:800 is needed if the font-size is set to 200% in CSS:
 		//this.marker.bindPopup(popuptext, { maxWidth: 800 });
-		//this.marker.bindPopup(popuptext);
 		this.marker.bindPopup(this.presenter.getPopupText(initiative));
 
-		// Add the event handlers that are defined in model/pointseq:
-		//Object.keys(eventHandlers).forEach(function(k) {
 		const eventHandlers = this.presenter.getEventHandlers(initiative);
 		Object.keys(eventHandlers).forEach(function(k) {
 			this.marker.on(k, eventHandlers[k]);
 		}, this);
 
-		//this.parent = options.cluster ? group : map;
-		//this.parent = cluster;
 		this.cluster = unselectedCluster;
 		this.cluster.addLayer(this.marker);
 		markerForInitiative[initiative.uniqueId] = this;
