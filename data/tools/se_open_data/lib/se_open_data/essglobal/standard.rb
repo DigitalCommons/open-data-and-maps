961816b69b96bbc0ddb613d1a672e69fdd1f571c
Matt Wallis <matt.wallis@tickpea.com>
2019-02-07 12:10:49 +0000

    LOD is now generated for essglobal activities
    Relates to SolidarityEconomyAssociation/oxford-map#4

--- data/tools/se_open_data/lib/se_open_data/essglobal/standard.rb
+++ data/tools/se_open_data/lib/se_open_data/essglobal/standard.rb
@@ -3,44 +3,34 @@ require 'rdf/rdfxml'
 
 module SeOpenData
   module Essglobal
-    class LegalForm
-
-      # ################################
-      # This module may well be OBSOLETE
-      # ################################
-      # It has been replaced by SeOpenData::Eddglobal::Standard
-
-      def initialize(essglobal_uri)
-        uri = "#{essglobal_uri}standard/legal-form"
+    class Standard
+      def initialize(essglobal_uri, taxonomy)
+        # taxonomy is a string that matches one of the filenames (but without `.skos`) in:
+        # https://github.com/essglobal-linked-open-data/map-sse/tree/develop/vocabs/standard
+        uri = "#{essglobal_uri}standard/#{taxonomy}"
         puts uri
         graph = ::RDF::Graph.load(uri, format: :rdfxml)
-        #puts graph.dump(:ntriples)
-        #::RDF::Reader.each { |klass| puts klass.name }
-        #::RDF::Format.each { |klass| puts klass.name }
         query = ::RDF::Query.new do
           pattern [:concept, ::RDF.type, ::RDF::Vocab::SKOS.Concept]
           pattern [:concept, ::RDF::Vocab::SKOS.prefLabel, :label]
         end
         @lookup = {}
         query.execute(graph).each do |solution|
-          #puts "LegalForm: #{solution.label}"
-          #@lookup[solution.label.to_s.gsub(/ /, "")] = solution
           @lookup[to_key(solution.label.to_s)] = solution
         end
       end
       def has_label? (label)
-        #@lookup.has_key?(label.gsub(/ /, ""))
         @lookup.has_key?(to_key(label))
       end
       def concept_uri(label)
-        #@lookup[label.gsub(/ /, "")].concept.to_s
         @lookup[to_key(label)].concept.to_s
       end
+      private
       def to_key(label)
         label.upcase.gsub(/ /, "")
       end
-
     end
   end
 end
+
 
