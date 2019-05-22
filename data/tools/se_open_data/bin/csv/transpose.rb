c09f36e1eb6e75876f43e5bf1fd6416743a27e81
Matt Wallis <matt.wallis@tickpea.com>
2019-05-22 09:48:15 +0100

    Handle broken pipe properly
    e.g. when piping to head

--- data/tools/se_open_data/bin/csv/transpose.rb
+++ data/tools/se_open_data/bin/csv/transpose.rb
@@ -3,8 +3,11 @@ require 'csv'
 require 'pp'
 
 # Transpose a CSV file
-def transpose(input_io, output_io)
-  csv_opts = {}
+def transpose(input_io, output_io, csv_opts = {})
+
+  # See https://stackoverflow.com/a/30103307/685715:
+  Signal.trap("SIGPIPE", "SYSTEM_DEFAULT")
+
   csv_in = ::CSV.new(input_io, csv_opts)
   csv_out = ::CSV.new(output_io)
 
